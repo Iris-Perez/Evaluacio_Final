52fa7d675c1a1c5afefc2dcc442e3d3fb2d47b58
Iris Perez <irisperez9890@gmail.com>
2022-05-03 09:44:12 -0600

    Update main.py
    El programa funciona correctamente

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ print("Estudiante de la universidad Rafel Landivar")
 print("Iris Ivette Perez Pastora")
 print("Carnet: 1583822")
 print("18 años :)")
-print("Ingresa tu animal favorito")
 
+print("Ingresa tu animal favorito")
+animal=input()
+if animal == "Mariposas" or "mariposas":
+    print("A mí también me gustan las Mariposas :3")
+else:
+    print("Es un animal interesante, pero me gustan más las Mariposas")
